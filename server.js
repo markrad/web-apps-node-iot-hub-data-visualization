efe92415beaf375b848320e33de70a41285c58be
Mark Radbourne <markrad@msn.com>
2022-09-12 16:56:57 -0700

    Add video

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
 const EventHubReader = require('./scripts/event-hub-reader.js');
+const { WebSocketServer } = require('ws');
 
 const iotHubConnectionString = process.env.IotHubConnectionString;
 if (!iotHubConnectionString) {
@@ -27,22 +28,77 @@ app.use((req, res /* , next */) => {
 });
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
+const wss = new WebSocketServer({ noServer: true });
 
-wss.broadcast = (data) => {
-  wss.clients.forEach((client) => {
+function broadcast(data, clients) {
+  let needCleanup = false;
+  clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
       try {
-        console.log(`Broadcasting data ${data}`);
+        // console.log(`Broadcasting data ${data}`);
         client.send(data);
       } catch (e) {
         console.error(e);
       }
     }
-  });
-};
+    else {
+      needCleanup = true;
+    }
 
-server.listen(process.env.PORT || '3000', () => {
+    if (needCleanup) {
+      clients = clients.filter((client) => client.readyState == WebSocket.OPEN || client.readyState == WebSocket.CONNECTING);
+    }
+  });
+}
+
+var wss1 = [];      // IoT hub data
+var wss2 = [];      // Video stream
+var vidin = null;   // Video in
+
+wss.on('connection', (ws) => {
+  wss1.push(ws);
+  console.log('Data out connected');
+});
+
+wss.on('videoout', (ws) => {
+  wss2.push(ws);
+  console.log('Video out connected')
+});
+
+function videoSend(data) {
+  broadcast(data, wss2);
+}
+
+wss.on('videoin', (ws) => {
+  if (vidin) {
+    vidin.off('message', videoSend);
+  }
+  vidin = ws;
+  vidin.on('message', videoSend);
+  console.log('Video in connected');
+});
+
+server.on('upgrade', (request, socket, head) => {
+  if (request.url == '/') {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit('connection', ws, request);
+    });
+  }
+  else if (request.url == '/videoout') {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      console.log('video reader connected');
+      wss.emit('videoout', ws, request);
+    });
+  }
+  else if (request.url == '/videoin') {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      wss.emit('videoin', ws, request);
+    });
+  }
+})
+
+server.listen(process.env.PORT || '443', () => {
   console.log('Listening on %d.', server.address().port);
 });
 
@@ -50,16 +106,18 @@ const eventHubReader = new EventHubReader(iotHubConnectionString, eventHubConsum
 
 (async () => {
   await eventHubReader.startReadMessage((message, date, deviceId) => {
-    try {
-      const payload = {
-        IotData: message,
-        MessageDate: date || Date.now().toISOString(),
-        DeviceId: deviceId,
-      };
+    if (wss1) {
+      try {
+        const payload = {
+          IotData: message,
+          MessageDate: date || Date.now().toISOString(),
+          DeviceId: deviceId,
+        };
 
-      wss.broadcast(JSON.stringify(payload));
-    } catch (err) {
-      console.error('Error broadcasting: [%s] from [%s].', err, message);
+        broadcast(JSON.stringify(payload), wss1);
+      } catch (err) {
+        console.error('Error broadcasting: [%s] from [%s].', err, message);
+      }
     }
   });
 })().catch();
